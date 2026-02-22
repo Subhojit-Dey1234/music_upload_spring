44a1d080f09990d0e569bbbcfcd4f747965ba31f
Subhojit Dey <subhojit2000dey@gmail.com>
2026-02-22 22:36:25 +0530

    Removing the unused methods

--- src/main/java/com/musicupload/music/clone/security/JwtUtil.java
+++ src/main/java/com/musicupload/music/clone/security/JwtUtil.java
@@ -1,14 +1,12 @@
 package com.musicupload.music.clone.security;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
